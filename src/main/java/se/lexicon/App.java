02240f672b0e6f1c2f36fdc95b4a4708b5c639af
liljana ristevska <li.ristevska@gmail.com>
2022-10-18 14:56:45 +0200

    Java Length

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,22 @@
+package se.lexicon;
+
+/**
+ * Hello world!
+ */
+
+public class App {
+
+    public static void main(String[] args) {
+
+
+        String str = "Java";
+        int size = str.length();
+        System.out.println("String Length: " + size);
+
+    }
+
+
+}
+
+
+
---
+++ src/main/java/se/lexicon/String_Java.java
@@ -0,0 +1,15 @@
+package se.lexicon;
+
+public class String_Java {
+
+    public static void main(String[] args) {
+
+        String str = "Java";
+        int size = str.length();
+        System.out.println("String Length: " + size);
+
+
+    }
+
+
+}
